32f42c51b075224d28b925a6fe2b66da5d7412be
arifwahyudi01 <100467483+arifwahyudi01@users.noreply.github.com>
2023-11-29 00:00:24 +0700

    asset dan anggaran

---
+++ app/src/main/java/com/example/pengelolakeuangan/Anggaran1Activity.kt
@@ -0,0 +1,11 @@
+package com.example.pengelolakeuangan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Anggaran1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anggaran1)
+    }
+}
