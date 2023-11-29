23c11ffdf5d13d9a71bbb337b7f01db8cc6bab9f
arifwahyudi01 <100467483+arifwahyudi01@users.noreply.github.com>
2023-11-29 22:23:48 +0700

    anggram

--- app/src/main/java/com/example/pengelolakeuangan/Anggaran1Activity.kt
+++ app/src/main/java/com/example/pengelolakeuangan/Anggaran1Activity.kt
@@ -1,11 +1,36 @@
-package com.example.pengelolakeuangan
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class Anggaran1Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anggaran1)
+
+        val editTextBiaya = findViewById<EditText>(R.id.editTextBiaya)
+        val buttonHitung = findViewById<Button>(R.id.buttonHitung)
+        val textViewHasil = findViewById<TextView>(R.id.textViewHasil)
+
+        buttonHitung.setOnClickListener {
+            // Mendapatkan nilai biaya dari EditText
+            val biayaStr = editTextBiaya.text.toString()
+
+            if (biayaStr.isNotEmpty()) {
+                // Menghitung anggaran, misalnya dengan mengalikan biaya dengan faktor tertentu
+                val biaya = biayaStr.toDouble()
+                val faktor = 1.2 // Anda dapat mengganti faktor sesuai kebutuhan
+                val anggaran = biaya * faktor
+
+                // Menampilkan hasil di TextView
+                textViewHasil.text = "Anggaran: $$anggaran"
+            } else {
+                // Menampilkan pesan jika EditText kosong
+                textViewHasil.text = "Masukkan biaya terlebih dahulu."
+            }
+        }
     }
 }
