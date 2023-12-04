be6cbd7a4393e4110af45935d5b740ae77771796
arifwahyudi01 <100467483+arifwahyudi01@users.noreply.github.com>
2023-12-04 22:28:46 +0700

    anggaran , tapi gaada button di home

--- app/src/main/java/com/example/pengelolakeuangan/Anggaran1Activity.kt
+++ app/src/main/java/com/example/pengelolakeuangan/Anggaran1Activity.kt
@@ -1,11 +1,37 @@
-package com.example.pengelolakeuangan
+// Anggaran1Activity.kt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pengelolakeuangan.R
 
 class Anggaran1Activity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_anggaran1)
+
+        // Find buttons by ID
+        val btnTransaction = findViewById<Button>(R.id.btnTransaction)
+        val btnBudget = findViewById<Button>(R.id.btnBudget)
+        val btnMore = findViewById<Button>(R.id.btnMore)
+        val btnProfile = findViewById<Button>(R.id.btnProfile)
+
+        // Set onClickListeners for each button (you can implement your logic here)
+        btnTransaction.setOnClickListener {
+            // Handle transaction button click
+        }
+
+        btnBudget.setOnClickListener {
+            // Handle budget button click
+        }
+
+        btnMore.setOnClickListener {
+            // Handle more button click
+        }
+
+        btnProfile.setOnClickListener {
+            // Handle profile button click
+        }
     }
 }
--- app/src/main/java/com/example/pengelolakeuangan/Anggaran1ActivityFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.pengelolakeuangan;
-
-import android.app.Activity;
-
-public class Anggaran1ActivityFragment extends Activity {
-}
