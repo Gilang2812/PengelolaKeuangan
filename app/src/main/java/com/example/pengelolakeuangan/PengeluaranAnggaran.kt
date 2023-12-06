1921c7397826e12a60118a49673ec208719069b2
arifwahyudi01 <100467483+arifwahyudi01@users.noreply.github.com>
2023-12-07 03:02:07 +0700

    anngaran , backend alun

---
+++ app/src/main/java/com/example/pengelolakeuangan/PengeluaranAnggaran.kt
@@ -0,0 +1,11 @@
+package com.example.pengelolakeuangan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tambahPemasukan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pengeluaran_anggaran)
+    }
+}
