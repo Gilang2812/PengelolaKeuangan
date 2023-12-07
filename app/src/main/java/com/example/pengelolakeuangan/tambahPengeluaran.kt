0c270df126494594f1b9e2f9b993a3b9012dd4de
arifwahyudi01 <100467483+arifwahyudi01@users.noreply.github.com>
2023-12-07 13:44:44 +0700

    anggaran

--- app/src/main/java/com/example/pengelolakeuangan/tambahPengeluaran.kt
+++ app/src/main/java/com/example/pengelolakeuangan/tambahPengeluaran.kt
@@ -3,7 +3,7 @@ package com.example.pengelolakeuangan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class tambahPemasukan : AppCompatActivity() {
+class tambahPengeluaran : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tambah_pengeluaran)
