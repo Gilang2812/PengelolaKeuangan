b9254e312b6bc607f870d98663238ca9732102e1
gilangkh <127704769+gilangkh@users.noreply.github.com>
2023-12-17 09:30:30 +0700

    model

--- app/src/main/java/com/example/pengelolakeuangan/ApiService.kt
+++ app/src/main/java/com/example/pengelolakeuangan/ApiService.kt
@@ -1,18 +1,12 @@
 package com.example.pengelolakeuangan
 
 import com.example.pengelolakeuangan.adapter.*
+import com.google.gson.annotations.SerializedName
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.POST
-import java.util.Date
-import com.google.gson.annotations.SerializedName
 import retrofit2.http.*
+import java.util.Date
 
 private val retrofit = Retrofit.Builder()
     .baseUrl("https://ptbfahriganteng.et.r.appspot.com/")
@@ -56,35 +50,6 @@ interface ApiService {
     suspend fun getAset(@Header("Authorization") authorization: String) : List<Aset>
     @POST("users")
     suspend fun createUser(@Body user: User): Response<User>
-
-    data class User(
-        val id_user: String,
-        val id_daerah: String,
-        val nama: String,
-        val email: String,
-        val password: String,
-        val created_at: String,
-        val updated_at: String
-    )
-    data class Daerah (
-        val id_daerah: String,
-        val nama: String,
-        val umr :Int,
-        val created_at: String,
-        val updated_at: String
-    )
-    data class Transaksi(
-        val id_transaksi: String,
-        val id_user: String,
-        val id_kategori: String,
-        val id_jenis: String,
-        val id_aset: String,
-        val tanggal: Date,
-        val jumlah: Int,
-        val note: String,
-        val created_at: Date,
-        val updated_at: Date
-    )
 
     data class Kategori(
         @SerializedName("id_kategori")
