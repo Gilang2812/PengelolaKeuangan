88d481d90ec14a4bb003112210dbea8194e8ad32
gilangkh <gilang281201@gmail.com>
2023-12-26 18:17:47 +0700

    ini coba dlu arip

--- app/src/main/java/com/example/pengelolakeuangan/AssetActivity.kt
+++ app/src/main/java/com/example/pengelolakeuangan/AssetActivity.kt
@@ -2,10 +2,16 @@ package com.example.pengelolakeuangan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class AssetActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_asset)
+class AssetActivity : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
-}
