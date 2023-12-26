8b1204827e943dfed3152621caba22af0494faf6
arifwahyudi01 <100467483+arifwahyudi01@users.noreply.github.com>
2023-12-26 18:21:35 +0700

    ini coba dlu arip yang done kayanya

--- app/src/main/java/com/example/pengelolakeuangan/AssetActivity.kt
+++ app/src/main/java/com/example/pengelolakeuangan/AssetActivity.kt
@@ -5,14 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class AssetActivity : Fragment() {
+    private lateinit var fab: FloatingActionButton
+    private lateinit var recyclerView: RecyclerView
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_daily_list, container, false)
+        val token = SharedPreferencesUtil.retrieveTokenFromSharedPreferences(requireContext())
 
         return view
     }
